1dee3cedd1e6f49cfe02b07f22d43432841a733b
Rodrigo Barbosa <rodrigo.lima@agriconnected.com>
2020-07-30 09:54:37 -0300

    adjust for the send email

--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,5 +1,35 @@
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { ICreateUserRequestDTO } from "./CreateUserDTO";
+import { User } from "../../entities/User";
+import { IMailProvider } from "../../providers/IMailProvider";
+
 export class CreateUserUseCase {
-    async execute(){
-        
+  constructor(
+      private usersRepository: IUsersRepository,
+    private emailProvider: IMailProvider,
+      ) {}
+
+  async execute(data: ICreateUserRequestDTO) {
+    const userAlreadyExists = await this.usersRepository.findByEmail(
+      data.email
+    );
+    if (userAlreadyExists) {
+      throw new Error("Usuário já existe.");
     }
+    const user = new User(data);
+    await this.usersRepository.save(user);
+
+    this.emailProvider.sendMail({
+        to: {
+            name:data.name,
+            email: data.email,
+        }, 
+        from: {
+            name: 'Equipe do aplicativo',
+            email: 'noreply@equipe.com',
+        },
+        subject: "Seja bem vindo a plataforma",
+        body:'<p>Agora você já pode fazer login</p>'
+    })
+  }
 }
